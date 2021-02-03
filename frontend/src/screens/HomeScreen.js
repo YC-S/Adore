7513415e6704a34c30b05249f0fb82d9a2fdd28a
YC-S <yuanchenshi@gmail.com>
2021-02-03 17:31:25 -0500

    add back end express

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import products from '../products'
+import axios from 'axios'
 
 const HomeScreen = () => {
+	const [products, setProducts] = useState([])
+	useEffect(() => {
+		const fetchProducts = async () => {
+			const { data } = await axios.get('./api/products')
+			setProducts(data)
+		}
+		fetchProducts()
+	}, [])
 	let subscriptions = products.slice(0, 3)
 	let vases = products.slice(3, 10)
-	let supermeVases = products.slice(11)
+	let highEndVases = products.slice(11)
 	return (
 		<>
 			<h1>Subscriptions</h1>
@@ -27,7 +35,7 @@ const HomeScreen = () => {
 			</Row>
 			<h1>Adore Classics Vases</h1>
 			<Row>
-				{supermeVases.map((product) => (
+				{highEndVases.map((product) => (
 					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
 						<Product product={product} />
 					</Col>
