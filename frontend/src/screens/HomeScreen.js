fbaf4ff82fcc0603d71ec4da1d8041812d83eb99
YC-S <yuanchenshi@gmail.com>
2021-02-03 16:50:57 -0500

    update front end

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -4,11 +4,30 @@ import Product from '../components/Product'
 import products from '../products'
 
 const HomeScreen = () => {
+	let subscriptions = products.slice(0, 3)
+	let vases = products.slice(3, 10)
+	let supermeVases = products.slice(11)
 	return (
 		<>
-			<h1>Latest Products</h1>
+			<h1>Subscriptions</h1>
 			<Row>
-				{products.map((product) => (
+				{subscriptions.map((product) => (
+					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+						<Product product={product} />
+					</Col>
+				))}
+			</Row>
+			<h1>Monday Rocks Vases</h1>
+			<Row>
+				{vases.map((product) => (
+					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+						<Product product={product} />
+					</Col>
+				))}
+			</Row>
+			<h1>Adore Classics Vases</h1>
+			<Row>
+				{supermeVases.map((product) => (
 					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
 						<Product product={product} />
 					</Col>
--- frontend/src/vases.js
+++ frontend/src/vases.js
@@ -1,45 +1,6 @@
-const products = [
+const vases = [
 	{
 		_id: '1',
-		name: 'Monday Rocks',
-		image: '/images/Monday_Rocks.jpg',
-		description:
-			'DIY kit & 4-8 stems of premium fresh flowers picked and perfectly designed to put on a small working desk! They have the magic to fresh up your Mondays and be your rock for a hectic week!',
-		brand: 'Adore',
-		category: 'Flowers',
-		price: '4.99/week',
-		countInStock: 100,
-		rating: 4,
-		numReviews: 120,
-	},
-	{
-		_id: '2',
-		name: 'Adore Classics',
-		image: '/images/Adore_Classics.jpg',
-		description:
-			'DIY kit & 6-12 stems of premium fresh flowers picked and perfectly designed to create classic table flowers! Start your week with these beauties blooming and stretching in the air!',
-		brand: 'Adore',
-		category: 'Flowers',
-		price: '9.99/week',
-		countInStock: 100,
-		rating: 4.5,
-		numReviews: 56,
-	},
-	{
-		_id: '3',
-		name: 'Brightest Bloom',
-		image: '/images/Brightest_Bloom.jpg',
-		description:
-			'DIY kit & 12-24 stems of premium fresh flowers picked and designed to create a large bouquet, perfect to decorate a spacious living room!',
-		brand: 'Adore',
-		category: 'Flowers',
-		price: '19.99/week',
-		countInStock: 100,
-		rating: 5,
-		numReviews: 3,
-	},
-	{
-		_id: '4',
 		name: "Dora Bud Vase 5.5'",
 		image: '/images/vases/dora_bud_vase.jpg',
 		description:
@@ -53,7 +14,7 @@ const products = [
 		numReviews: 120,
 	},
 	{
-		_id: '5',
+		_id: '2',
 		name: "Hurricane Glass Vase 6.5'",
 		image: '/images/vases/hurricane_glass_vase.jpg',
 		description:
@@ -67,7 +28,7 @@ const products = [
 		numReviews: 120,
 	},
 	{
-		_id: '6',
+		_id: '3',
 		name: "Sleeping Beauty Mini Pot'",
 		image: '/images/vases/sleeping_beauty_mini_pot.jpg',
 		description:
@@ -81,7 +42,7 @@ const products = [
 		numReviews: 120,
 	},
 	{
-		_id: '7',
+		_id: '4',
 		name: 'Blue Mini Footed Pot',
 		image: '/images/vases/blue_mini_footed_pot.jpg',
 		description:
@@ -95,9 +56,9 @@ const products = [
 		numReviews: 120,
 	},
 	{
-		_id: '8',
+		_id: '5',
 		name: "Mini 3.15' Ceramic Pot",
-		image: '/images/vases/mini_ceramic_pot.jpg',
+		image: '/images/vases/dora_bud_vase.jpg',
 		description:
 			'Available only to Adore Flower Subscribers.Clear glass vase, perfect for Monday Rocks.',
 		dimension: '5.5” (H) x 3” (W) x 1.75',
@@ -109,7 +70,7 @@ const products = [
 		numReviews: 120,
 	},
 	{
-		_id: '9',
+		_id: '6',
 		name: "Modern Creative Cube 3'",
 		image: '/images/vases/modern_creative_cube.jpg',
 		description:
@@ -123,7 +84,7 @@ const products = [
 		numReviews: 120,
 	},
 	{
-		_id: '10',
+		_id: '7',
 		name: 'Pink Mini Footed Pot',
 		image: '/images/vases/pink_mini_footed_pot.jpg',
 		description:
@@ -137,7 +98,7 @@ const products = [
 		numReviews: 120,
 	},
 	{
-		_id: '11',
+		_id: '8',
 		name: 'Handmade Wood Cube',
 		image: '/images/vases/Handmade_wood_cube.jpg',
 		description:
@@ -151,7 +112,7 @@ const products = [
 		numReviews: 120,
 	},
 	{
-		_id: '12',
+		_id: '9',
 		name: 'Birch Bark Cube',
 		image: '/images/vases/birch_bark_cube.jpg',
 		description:
@@ -165,7 +126,7 @@ const products = [
 		numReviews: 120,
 	},
 	{
-		_id: '13',
+		_id: '10',
 		name: 'Wood Drawer Cube',
 		image: '/images/vases/wood_drawer_cube.jpg',
 		description:
@@ -179,7 +140,7 @@ const products = [
 		numReviews: 120,
 	},
 	{
-		_id: '14',
+		_id: '11',
 		name: 'White Ceramic Pot',
 		image: '/images/vases/white_ceramic_pot.jpg',
 		description:
@@ -194,4 +155,4 @@ const products = [
 	},
 ]
 
-export default products
+export default vases
