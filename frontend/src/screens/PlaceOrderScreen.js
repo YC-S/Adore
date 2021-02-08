1a79f8e822b59532e23d59aacd0fd58c4e015694
YC-S <yuanchenshi@gmail.com>
2021-02-08 12:16:33 -0500

    create order

--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -4,13 +4,11 @@ import { Button, Row, Col, ListGroup, Image, Card } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import CheckoutSteps from '../components/CheckoutSteps'
+import { createOrder } from '../actions/orderActions'
 
 const PlaceOrderScreen = ({ history }) => {
 	const dispatch = useDispatch()
 
-	const placeOrderHandler = () => {
-		console.log('order')
-	}
 	const cart = useSelector((state) => state.cart)
 
 	// Calculate prices
@@ -29,6 +27,30 @@ const PlaceOrderScreen = ({ history }) => {
 	cart.totalPrice = addDecimals(
 		Number(cart.itemsPrice) + Number(cart.shippingPrice) + Number(cart.taxPrice)
 	)
+
+	const orderCreate = useSelector((state) => state.orderCreate)
+	const { order, success, error } = orderCreate
+
+	useEffect(() => {
+		if (success) {
+			history.push(`/order/${order._id}`)
+		}
+		// eslint-disable-next-line
+	}, [history, success])
+
+	const placeOrderHandler = () => {
+		dispatch(
+			createOrder({
+				orderItems: cart.cartItems,
+				shippingAddress: cart.shippingAddress,
+				paymentMethod: cart.paymentMethod,
+				itemsPrice: cart.itemsPrice,
+				shippingPrice: cart.shippingPrice,
+				taxPrice: cart.taxPrice,
+				totalPrice: cart.totalPrice,
+			})
+		)
+	}
 	return (
 		<>
 			<CheckoutSteps step1 step2 step3 step4 />
@@ -113,6 +135,9 @@ const PlaceOrderScreen = ({ history }) => {
 									<Col>Total</Col>
 									<Col>${cart.totalPrice}</Col>
 								</Row>
+							</ListGroup.Item>
+							<ListGroup.Item>
+								{error && <Message variant='danger'>{error}</Message>}
 							</ListGroup.Item>
 							<ListGroup.Item>
 								<Button
