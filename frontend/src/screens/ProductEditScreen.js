6a16bdb0943f0065aebd61e7f330b660a78d8f39
YC-S <yuanchenshi@gmail.com>
2021-02-09 10:08:33 -0500

    admin update products

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -33,6 +33,9 @@ const ProductEditScreen = ({ match, history }) => {
 		success: successUpdate,
 	} = productUpdate
 
+	const userLogin = useSelector((state) => state.userLogin)
+	const { userInfo } = userLogin
+
 	useEffect(() => {
 		if (successUpdate) {
 			dispatch({ type: PRODUCT_UPDATE_RESET })
@@ -61,6 +64,7 @@ const ProductEditScreen = ({ match, history }) => {
 		try {
 			const config = {
 				headers: {
+					Authorization: `Bearer ${userInfo.token}`,
 					'Content-Type': 'multipart/form-data',
 				},
 			}
