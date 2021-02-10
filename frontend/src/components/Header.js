54f02d60a85d12778d2f6c6430529720886ff3c9
YC-S <yuanchenshi@gmail.com>
2021-02-09 23:04:37 -0500

    add product search

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap'
+import SearchBox from './SearchBox'
 import { logout } from '../actions/userActions'
 
 const Header = () => {
@@ -18,11 +20,12 @@ const Header = () => {
 			<Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
 				<Container>
 					<LinkContainer to='/'>
-						<Navbar.Brand>Adore Table Flower</Navbar.Brand>
+						<Navbar.Brand>Adore Flower</Navbar.Brand>
 					</LinkContainer>
 					<Navbar.Toggle aria-controls='basic-navbar-nav' />
 					<Navbar.Collapse id='basic-navbar-nav'>
-						<Nav className='ml-auto px-2'>
+						<Route render={({ history }) => <SearchBox history={history} />} />
+						<Nav className='ml-auto'>
 							<LinkContainer to='/cart'>
 								<Nav.Link>
 									<i className='fas fa-shopping-cart'></i> Cart
